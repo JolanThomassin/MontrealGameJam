2db35fed878bec0ddf5a27bd420bf02eff346328
Jian Jia <jian.jia.1@ens.etsmtl.ca>
2023-04-28 21:39:20 -0400

    script

---
+++ Assets/Prefabs/Projectile/Attack.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+
+// An abstract class for bullets
+// will be inherited by real bullet class
+public abstract class Attack : MonoBehaviour
+{
+    public int damage;
+    public int speed;
+    public Animator animator;
+
+    [SerializeField]
+    protected Vector3 direction;  // the direction of this attack
+
+    
+    /// <summary>
+    /// Function to set the shooting direction of this bullet
+    /// </summary>
+    /// <param name="direction"></param>
+    public virtual void setDirection(Vector3 direction)
+    {
+        this.direction = direction;
+    }
+
+    protected abstract void OnTriggerEnter2D(Collider2D collision);
+
+}
---
+++ Assets/Prefabs/Projectile/Player_Bullet1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Bullet1 : Bullet
+{
+    public float maxDistance;  // max distance of this bullet can reach
+    public bool moveOnStart = false;
+    public bool test = false;
+
+    Vector3 initialPos;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        initialPos = transform.position;
+        base.Start();  // call the start() in the base class
+        if (moveOnStart)
+            Move(this.transform.position, direction, maxDistance);  // start moving this bullet (coroutine)
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (test)
+        {
+            test = false;
+            transform.position = initialPos;
+            Move(this.transform.position, direction, maxDistance);
+        }
+    }
+
+    /// <summary>
+    /// This function will be called automatically when the bullet is colliding with any collider
+    /// </summary>
+    /// <param name="collision"></param>
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.gameObject.tag == "Monster" || collision.gameObject.tag == "Ground")
+        //{
+        //    if (collision.gameObject.tag == "Monster") {
+        //       // if (collision.gameObject.GetComponent<Character>().isInvincible) {
+        //            Debug.Log("monster dodged");
+        //       //     return;
+        //   // }
+        //    }
+        //    stop();  // stop the motion of bullet when hits a monster
+        //}
+    }
+
+
+
+
+}
