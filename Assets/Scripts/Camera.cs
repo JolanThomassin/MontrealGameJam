be136b1ed46fedb1b8e73a8004648e5ae3ab8926
Jian Jia <jian.jia.1@ens.etsmtl.ca>
2023-04-29 16:02:04 -0400

    simple follow camera

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class Camera : MonoBehaviour
+{
+    public GameObject followTarget;
+
+    private void Update()
+    {
+        float z = transform.position.z;
+        transform.position = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, z);
+    }
+
+}
