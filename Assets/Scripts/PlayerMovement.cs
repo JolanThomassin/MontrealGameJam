88f591e1bbe508302fb081ab76917d532881fcbc
Manoël Nohra <manoel.nohra@gmail.com>
2023-04-28 21:39:16 -0400

    Character got collision with the map

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidbody2d;
-    private float movementValue = 10f;
+    private float movementValue = 5f;
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
