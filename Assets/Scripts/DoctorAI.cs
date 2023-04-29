5def509104eefd599d6e3b3390b26559fb266ee8
Baptiste <un>
2023-04-28 22:47:16 -0400

    IA Doctor v3

--- Assets/Scripts/DoctorAI.cs
+++ Assets/Scripts/DoctorAI.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DoctorAI : MonoBehaviour
 {
 
-    private float speed = 50f;
+    private float speed = 10f;
 
     //-1 = Gauche, 0 = Bouge pas, 1 = Droite
     private int chosenDirectionX;
@@ -15,6 +15,10 @@ public class DoctorAI : MonoBehaviour
 
     private Rigidbody2D rigidbody;
     Vector2 movementVector = Vector2.zero;
+
+    public Rigidbody2D[] objectives;
+    float testX = 3.5f;
+    float testY = -2.69f;
 
     //public TilemapCollider2D walls;
 
@@ -31,22 +35,46 @@ public class DoctorAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(timerDecision%30 == 0) {
-            
+        if(timerDecision%1500 == 0) {
+            Decision();
+            /*
             chosenDirectionX = Random.Range(-1,2);
             chosenDirectionY = Random.Range(-1,2);
             while(chosenDirectionX == 0 && chosenDirectionY == 0) {
                 chosenDirectionX = Random.Range(-1,2);
                 chosenDirectionY = Random.Range(-1,2);
-            }
+            }*/
         }
         
+        float memoPosX = movementVector.x;
+        float memoPosY = movementVector.y;
 
         movementVector.x = chosenDirectionX * Time.deltaTime * this.speed;
         movementVector.y = chosenDirectionY * Time.deltaTime * this.speed;
 
-        rigidbody.MovePosition(rigidbody.position + movementVector);
+        
+
+        movementVector.Normalize();
+
+        if(memoPosX == movementVector.x || memoPosY == movementVector.y || (memoPosX == movementVector.x && memoPosY == movementVector.y)) {
+            timerDecision = -1;
+        }
+        rigidbody.velocity = movementVector * this.speed;
 
         timerDecision++;
+    }
+
+    void Decision() {
+        if(testX > rigidbody.position.x) {
+            chosenDirectionX = 1;
+        }else if(testX < rigidbody.position.x) {
+            chosenDirectionX = -1;
+        }
+
+        if(testY > rigidbody.position.y) {
+            chosenDirectionY = 1;
+        }else if(testY < rigidbody.position.y) {
+            chosenDirectionY = -1;
+        }
     }
 }
