3357bc0be30e289478976bc4914c53a62c8a3578
Manoël Nohra <manoel.nohra@gmail.com>
2023-04-28 20:52:54 -0400

    Player can moove the capsule

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidbody2d;
-    private float movementValue = 1f;
+    private float movementValue = 10f;
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
@@ -19,21 +19,10 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move()
     {
-        if(Input.GetKeyDown(KeyCode.Z))
-        {
-            rigidbody2d.AddForce(rigidbody2d.velocity + new Vector2(0, movementValue));
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            rigidbody2d.AddForce(rigidbody2d.velocity + new Vector2(movementValue,0));
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            rigidbody2d.AddForce(rigidbody2d.velocity + new Vector2(-movementValue, 0));
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            rigidbody2d.AddForce(rigidbody2d.velocity + new Vector2(0, -movementValue));
-        }
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 vec= new Vector2(horizontal, vertical);
+        vec.Normalize();
+        rigidbody2d.velocity = vec * movementValue;
     }
 }
